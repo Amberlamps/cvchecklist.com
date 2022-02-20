d20b36b1cec8481bb0443d9581f7af9c708acf49
Alexander Behrens <alexander.behrens@speakap.nl>
2022-02-20 12:49:49 +0100

    small update

--- src/app.tsx
+++ src/app.tsx
@@ -15,19 +15,7 @@ function App(): JSX.Element {
         <ThemeProvider theme={defaultTheme}>
             <div className="App">
                 <Header>checklist</Header>
-                <header className="App-header">
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React wdafdsadsdf
-                    </a>
-                </header>
+                Hello
             </div>
         </ThemeProvider>
     );
